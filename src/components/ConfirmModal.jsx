a4fb188d9f2395018a1b86d9654094aa1f0f920f
shriramcai <Shriram.Shanbhag@cai-solutions.com>
2024-06-28 23:05:29 +0530

    Added Confirmation popup and Loader

---
+++ src/components/ConfirmModal.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from "@chakra-ui/react";
+
+export const ConfirmDeleteModal = ({ isOpen, onClose, onConfirm }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Confirm Deletion</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>Are you sure you want to delete this card?</ModalBody>
+        <ModalFooter>
+          <Button colorScheme="red" mr={3} onClick={onConfirm}>
+            Delete
+          </Button>
+          <Button variant="ghost" onClick={onClose}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
